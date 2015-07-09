c4e7da69c67d755e35ef8339c6a78148b88e89e1
liciicyi <liciicyi@outlook.com>
2015-07-09 11:13:29 +0800

    finished WebView & Http Test

--- WebViewTest/app/src/main/java/com/alan/root/webviewtest/MainActivity.java
+++ WebViewTest/app/src/main/java/com/alan/root/webviewtest/MainActivity.java
@@ -1,17 +1,61 @@
 package com.alan.root.webviewtest;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    public static final int SHOW_RESPONSE = 0;
+
+    private Button btnSendRequest, btnhttpClient;
+    private TextView responseText;
+
+    private Handler handler = new Handler() {
+
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case SHOW_RESPONSE:
+                    String response = (String) message.obj;
+                    responseText.setText(response);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSendRequest = (Button) findViewById(R.id.btnSendRequest);
+        responseText = (TextView) findViewById(R.id.response);
+        btnSendRequest.setOnClickListener(this);
+
+        btnhttpClient = (Button) findViewById(R.id.btnhttpClient);
+        btnhttpClient.setOnClickListener(this);
+
     }
 
     @Override
@@ -34,5 +78,85 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSendRequest:
+//                sendRequestWithHttpURLConnection();
+                break;
+
+            case R.id.btnhttpClient:
+                sendRequestWithHttpClient();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /*private void sendRequestWithHttpURLConnection() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpsURLConnection connection = null;
+                try {
+                    URL url = new URL("https://www.baidu.com");
+                    connection = (HttpsURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    Message message = new Message();
+                    message.what = SHOW_RESPONSE;
+
+                    message.obj = response.toString();
+                    handler.handleMessage(message);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+
+    }*/
+
+    private void sendRequestWithHttpClient() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet("http://www.baidu.com");
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                        HttpEntity entity = httpResponse.getEntity();
+                        String response = EntityUtils.toString(entity,
+                                "utf-8");
+
+                        Message message = new Message();
+                        message.what = SHOW_RESPONSE;
+                        message.obj = response.toString();
+                        handler.sendMessage(message);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
